8dba46a103ac01c9b99405901c4f0088b6ea7d4a
GautamSridhar <gautam.neon@gmail.com>
2016-06-23 12:10:24 +0530

    Update to include testing and cross validation

--- train.m
+++ train.m
@@ -1,14 +1,4 @@
-%% CS294A/CS294W Programming Assignment Starter Code
-
-%  Instructions
-%  ------------
-% 
-%  This file contains code that helps you get started on the
-%  programming assignment. You will need to complete the code in sampleIMAGES.m,
-%  sparseAutoencoderCost.m and computeNumericalGradient.m. 
-%  For the purpose of completing the assignment, you do not need to
-%  change the code in this file. 
-%
+%% Comment out whichever step to switch between either training or cross validation.
 %%======================================================================
 %% STEP 0: Here we provide the relevant parameters values that will
 %  allow your sparse autoencoder to get good filters; you do not need to 
@@ -29,7 +19,7 @@ beta = 3;            % weight of sparsity penalty term
 %  display a random sample of 200 patches from the dataset
 
 patches = sampleIMAGES;
-%display_network(patches(:,randi(size(patches,2),200,1)),8);
+display_network(patches(:,randi(size(patches,2),200,1)),8);
 
 
 %  Obtain random parameters theta
@@ -37,24 +27,6 @@ theta = initializeParameters(hiddenSize, visibleSize);
 
 %%======================================================================
 %% STEP 2: Implement sparseAutoencoderCost
-%
-%  You can implement all of the components (squared error cost, weight decay term,
-%  sparsity penalty) in the cost function at once, but it may be easier to do 
-%  it step-by-step and run gradient checking (see STEP 3) after each step.  We 
-%  suggest implementing the sparseAutoencoderCost function using the following steps:
-%
-%  (a) Implement forward propagation in your neural network, and implement the 
-%      squared error term of the cost function.  Implement backpropagation to 
-%      compute the derivatives.   Then (using lambda=beta=0), run Gradient Checking 
-%      to verify that the calculations corresponding to the squared error cost 
-%      term are correct.
-%
-%  (b) Add in the weight decay term (in both the cost function and the derivative
-%      calculations), then re-run Gradient Checking to verify correctness. 
-%
-%  (c) Add in the sparsity penalty term, then re-run Gradient Checking to 
-%      verify correctness.
-%
 %  Feel free to change the training settings when debugging your
 %  code.  (For example, reducing the training set size or 
 %  number of hidden units may make your code run faster; and setting beta 
@@ -75,21 +47,21 @@ theta = initializeParameters(hiddenSize, visibleSize);
 % First, lets make sure your numerical gradient computation is correct for a
 % simple function.  After you have implemented computeNumericalGradient.m,
 % run the following: 
-%checkNumericalGradient();
+checkNumericalGradient();
 
 % Now we can use it to check your cost function and derivative calculations
 % for the sparse autoencoder.  
-%numgrad = computeNumericalGradient( @(x) sparseAutoencoderCost(x, visibleSize, ...
+numgrad = computeNumericalGradient( @(x) sparseAutoencoderCost(x, visibleSize, ...
 %                                                  hiddenSize, lambda, ...
 %                                                  sparsityParam, beta, ...
 %                                                 patches), theta);
 
 % Use this to visually compare the gradients side by side
-%disp([numgrad grad]); 
+disp([numgrad grad]); 
 
 % Compare numerically computed gradients with the ones obtained from backpropagation
-%diff = norm(numgrad-grad)/norm(numgrad+grad);
-%disp(diff); % Should be small. In our implementation, these values are
+diff = norm(numgrad-grad)/norm(numgrad+grad);
+disp(diff); % Should be small. In our implementation, these values are
             % usually less than 1e-9.
 
             % When you got this working, Congratulations!!! 
@@ -104,11 +76,9 @@ theta = initializeParameters(hiddenSize, visibleSize);
 
 %  Use minFunc to minimize the function
 %addpath minFunc/
-%options.Method = 'lbfgs'; % Here, we use L-BFGS to optimize our cost
-                          % function. Generally, for minFunc to work, you
-                          % need a function pointer with two outputs: the
-                          % function value and the gradient. In our problem,
-                          % sparseAutoencoderCost.m satisfies this.
+%options.Method = 'lbfgs'; % Here, we use conjugate gradient to optimize our cost
+                          % function. L-BFGS can also be used as in the original exercise 
+                          % code
 options = optimset('MaxIter', 400);	  % Maximum number of iterations of L-BFGS to run 
 %options.display = 'on';
 
@@ -125,5 +95,14 @@ options = optimset('MaxIter', 400);	  % Maximum number of iterations of L-BFGS t
 W1 = reshape(opttheta(1:hiddenSize*visibleSize), hiddenSize, visibleSize);
 display_network(W1', 12); 
   % save the visualization to a file 
-
+%%======================================================================
+%% STEP 6: Prediction
+testData = imread('');
+output = feedForwardAutoencoder(opttheta, hiddenSize, visibleSize, testData);
+output_image = reshape(output, [21 21]);
+%%======================================================================
+%% OPTIONAL: Cross Validation
+[error_train,error_val] = crossValidate(X_train,X_val,lambda, sparsityParam, beta,...
+                                                   hiddenSize,visibleSize);
+                                                   
 
