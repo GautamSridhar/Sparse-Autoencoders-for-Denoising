1784736aeac2b4f8bb6c70b0fb91da79b9c7a00a
GautamSridhar <gautam.neon@gmail.com>
2016-06-23 12:01:17 +0530

    Calculate the Cross Validation error.

---
+++ crossValidate.m
@@ -0,0 +1,26 @@
+% Function for calculating the training and cross validation errors
+% over different values of the training set to choose the correct
+% amount of data for training and choose appropriate hyperparameters
+function [error_train,error_val] = crossValidate(X_train,X_val,lambda, sparsityParam, beta,...
+                                                   hiddenSize,visibleSize)
+m = size(X_train,2);
+error_train = zeros(m, 1);
+error_val   = zeros(m, 1);
+for i=1:m
+    theta = initializeParameters(hiddenSize, visibleSize);
+    costFunc = @(p) sparseAutoencoderCost(p,visibleSize,hiddenSize,lambda, sparsityParam, beta,X_train(:,1:m));
+    options = optimset('MaxIter', 600);
+    [opttheta,~] = fmincg(costFunc,theta,options);
+    [error_train(i),~] = sparseAutoencoderCost(opttheta,visibleSize,hiddenSize,0,sparsityParam,0,X_train(:,1:i));
+    [error_val(i),~] = sparseAutoencoderCost(opttheta,visibleSize,hiddenSize,0,sparsityParam,0,X_val);
+end
+plot(1:m, error_train, 1:m, error_val);
+title('Learning curve')
+legend('Train', 'Cross Validation')
+xlabel('Number of training examples')
+ylabel('Error')
+axis([0 13 0 150])            %can be modified accordingly
+    
+    
+
+
