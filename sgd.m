26016e30cee94f7221c1da9947f0d54566d92753
Gautam Sridhar <gautam.neon@gmail.com>
2016-07-04 11:08:45 +0530

    updates sgd

--- sgd.m
+++ sgd.m
@@ -1,13 +1,14 @@
-function [optTheta,cost] = sgd(batchsize,alpha,gamma,theta, visibleSize, hiddenSize, ...
-                                             lambda, sparsityParam, beta, data,train_type)
+function [optTheta,cost] = sgd(batchsize,alpha,theta, visibleSize, hiddenSize, ...
+                                             lambda, sparsityParam, beta, data,data_n)
 theta_int = theta;
 m = size(data,2);
 v = zeros(size(theta));
 figure; hold on;
 axis([0 m 0 50]);
 for i=1:batchsize:m
+    data_n_int = data_n(:,i:i+batchsize,:);
     data_int = data(:,i:i+batchsize,:);
-    [cost,grad] = sparseAutoencoderCost(theta_int,visibleSize,hiddenSize,lambda,sparsityParam,beta,data_int,train_type);
+    [cost,grad] = sparseAutoencoderCost(theta_int,visibleSize,hiddenSize,lambda,sparsityParam,beta,data_int,data_n_int);
     thet = alpha.*grad;
     theta_out = theta_int - thet;
     theta_int = theta_out;
